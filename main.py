8d5c31a4c2f6dcdbbd0056f4240817d31a78d64d
GoStrong11 <frenlik20@gmail.com>
2024-03-11 16:19:27 +0200

    Перша версія програми

---
+++ main.py
@@ -0,0 +1,28 @@
+from googletrans import Translator
+
+def TransLate(text, lang):
+    translator = Translator()
+    try:
+        translated_text = translator.translate(text, dest=lang).text
+        return translated_text
+    except Exception as e:
+        return f"Translation error: {e}"
+
+def LangDetect(txt):
+    translator = Translator()
+    try:
+        detected = translator.detect(txt)
+        return f"Detected(lang={detected.lang}, confidence={detected.confidence})"
+    except Exception as e:
+        return f"Language detection error: {e}"
+
+def CodeLang(lang):
+    return lang.lower()
+
+if __name__ == "__main__":
+    txt = "Доброго дня. Як справии?"
+    lang = "en"
+    print(txt)
+    print(LangDetect(txt))
+    print(TransLate(txt, lang))
+    print(CodeLang(lang))
